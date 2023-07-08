c100acbd62427f7cbbba96ae4e09ce8fba95e706
KPGhat <kpghat@gmail.com>
2023-07-08 22:42:05 +0800

    Add fingerprint

--- rules/rules.go
+++ rules/rules.go
@@ -1,1 +1,28 @@
 package rules
+
+import (
+	"errors"
+	"io"
+	"strings"
+)
+
+func InitDatabaseFS(fs io.Reader) (n int, lastErr error) {
+	sourceBuf, err := io.ReadAll(fs)
+	if err != nil {
+		return 0, err
+	}
+	source := strings.Split(string(sourceBuf), "\n")
+	for _, line := range source {
+		line = strings.TrimSpace(line)
+		r := strings.SplitAfterN(line, "\t", 2)
+		if len(r) != 2 {
+			lastErr = errors.New(line + "invalid")
+			continue
+		}
+		err := add(r[0], r[1])
+		if err != nil {
+			lastErr = err
+		}
+	}
+	return len(source), lastErr
+}
--- scan/gonmap/gonmap_test.go
+++ scan/gonmap/gonmap_test.go
@@ -8,18 +8,8 @@ import (
 
 func TestScan(t *testing.T) {
 	var scanner = New()
-	host := "192.168.100.144"
-	port := 5001
+	host := "45.126.125.13"
+	port := 80
 	status, response := scanner.ScanTimeout(host, port, time.Second*30)
-	fmt.Println(status, response.FingerPrint.Service, host, ":", port)
-	port = 22
-	status, response = scanner.ScanTimeout(host, port, time.Second*30)
-
-	fmt.Println(status, response.FingerPrint.Service, host, ":", port)
-	port = 5000
-	status, response = scanner.ScanTimeout(host, port, time.Second*30)
-	fmt.Println(status, response.FingerPrint.Service, host, ":", port)
-	port = 445
-	status, response = scanner.ScanTimeout(host, port, time.Second*30)
 	fmt.Println(status, response.FingerPrint.Service, host, ":", port)
 }
